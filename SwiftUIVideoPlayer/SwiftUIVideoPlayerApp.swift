b98a729daf2e0111290e905dec2aa0b9d530f0cf
Scott Kellicker <scott@blankcanvas.video>
2023-08-01 16:39:55 -0400

    Initial Commit

---
+++ SwiftUIVideoPlayer/SwiftUIVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVideoPlayerApp.swift
+//  SwiftUIVideoPlayer
+//
+//  Created by scott on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
